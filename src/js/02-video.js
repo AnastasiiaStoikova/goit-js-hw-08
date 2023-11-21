733d7f81d88139d950e0e7fc5ecc69b88f7dc313
AnastasiiaStoikova <stoickova.nastya21@gmail.com>
2023-11-21 11:08:52 +0200

    last commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player(document.getElementById('vimeo-player'));
+
+
+function initPlayer() {
+
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+
+ 
+  if (currentTime) {
+    player.setCurrentTime(parseFloat(currentTime));
+  }
+
+  
+  player.on('timeupdate', throttle(() => {
+    player.getCurrentTime().then(current => {
+      localStorage.setItem('videoplayer-current-time', current.toString());
+    });
+  }, 1000));
+}
+
+document.addEventListener('DOMContentLoaded', initPlayer);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+
+const localStorageKey = 'feedback-form-state';
+
+// Отслеживаем событие input на форме
+form.addEventListener('input', throttle(handleFormInput, 500));
+
+// При загрузке страницы проверяем состояние хранилища
+window.addEventListener('load', () => {
+  const savedState = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+
+  // Заполняем поля формы сохраненными значениями
+  emailInput.value = savedState.email || '';
+  messageTextarea.value = savedState.message || '';
+});
+
+// При сабмите формы очищаем хранилище и выводим данные в консоль
+form.addEventListener('submit', handleSubmit);
+
+function handleFormInput() {
+  // Сохраняем текущие значения полей в локальное хранилище
+  const currentState = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(currentState));
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  // Получаем данные из хранилища и выводим в консоль
+  const savedState = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+  console.log('Form submitted with data:', {
+    email: savedState.email,
+    message: savedState.message,
+  });
+
+  // Очищаем хранилище и поля формы
+  localStorage.removeItem(localStorageKey);
+  emailInput.value = '';
+  messageTextarea.value = '';
+}
